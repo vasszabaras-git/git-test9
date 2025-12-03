2de644695ac05d621fd5295253fc217f9a97782c
vzamparas <vass.zabaras@gmail.com>
2025-12-03 16:13:46 +0100

    whatever

---
+++ src/gr/aueb/cf/testgit/DivisionApp.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.testgit;
+
+public class DivisionApp {
+    static void main() {
+        int a = 10;
+        int b = 2;
+        int c = 3;
+        System.out.println(a/c);
+        System.out.println(b/c);
+    }
+}
